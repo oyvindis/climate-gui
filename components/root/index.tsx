0bda4354d606e06cf5670b5d27ba873fe6903cdc
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-09-21 12:58:13 +0200

    feat: add refresh_token

--- components/root/index.tsx
+++ components/root/index.tsx
@@ -1,11 +1,20 @@
-import React, { memo, FC, PropsWithChildren } from 'react';
+import React, { memo, FC, PropsWithChildren, useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 
 import SC from './styled';
 
 interface Props {}
 
-const Root: FC<PropsWithChildren<Props>> = ({ children }) => (
-  <SC.Root>{children}</SC.Root>
-);
+const Root: FC<PropsWithChildren<Props>> = ({ children }) => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session?.error === 'RefreshAccessTokenError') {
+      signIn();
+    }
+  }, [session]);
+
+  return <SC.Root>{children}</SC.Root>;
+};
 
 export default memo(Root);
