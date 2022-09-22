6610485e7b8613b0485071042b46c86933af555a
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-09-22 20:09:05 +0200

    feat: use accessToken in request headers

--- services/api/climate/host.ts
+++ services/api/climate/host.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { getSession } from 'next-auth/react';
+
 import env from '../../../env';
 
 interface Props {
@@ -10,15 +12,22 @@ interface Props {
 
 const { READING_BASE_URI } = env;
 
-export const climateReading = ({ path, method, data, params }: Props) =>
-  axios({
-    url: `${READING_BASE_URI}${path}`,
-    method,
-    data,
-    params
-  })
-    .then(response => response.data)
-    .catch(() => null);
+export const climateReading = async ({ path, method, data, params }: Props) => {
+  const session: any = await getSession();
+  return (
+    axios({
+      url: `${READING_BASE_URI}${path}`,
+      method,
+      data,
+      params,
+      headers: {
+        Authorization: `Bearer ${session?.accessToken}`
+      }
+    })
+      .then(response => response.data)
+      .catch(() => null)
+  );
+}
 
 export const climateReadingPost = (path: string, body: any) =>
   climateReading({ path, method: 'POST', data: body });
